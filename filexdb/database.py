41cbbd16b06b92cb03d5ba32ddbcfa0a628085e0
Sam <samratbiswas986@gmail.com>
2023-02-22 03:41:16 +0530

    Modified db name & dir

--- filexdb/database.py
+++ filexdb/database.py
@@ -5,12 +5,25 @@ from .collection import Collection
 
 class FileXdb:
 
-    def __init__(self, db_name):
-        self.database = {}
-        self.db_name = db_name
+    def __init__(self, db_name: str, data_dir: str | None):
+        """
+        Creates a Databased in ``data_dir`` Directory named ``db_name``.
 
-    def collection(self, col_name) -> Collection:
-        collection = Collection(self.database, self.db_name, col_name)
+        If Database is already exists, interacts with that.
+
+        [Recommended]
+        Don't add any file extension. ``FileXdb`` will create its own file extension ``fxdb``.
+
+        :param db_name: Name of Database without file extension.
+        :param data_dir: Where the Database will be stored.
+        """
+
+        self._database = {}
+        self._db_name = db_name
+        self._data_dir = data_dir
+
+    def collection(self, col_name: str) -> Collection:
+        collection = Collection(self._database, self._db_name, col_name)
         # self.database[col_name] = collection
 
         return collection
