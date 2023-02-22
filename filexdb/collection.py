4499d0b016df2620301b7d4e9b298af60bf3cb02
Sam <samratbiswas986@gmail.com>
2023-02-22 08:18:30 +0530

    Fixed minor issues

--- filexdb/collection.py
+++ filexdb/collection.py
@@ -1,23 +1,47 @@
-from .document import Document
 import json
+
+from .document import Document
+from .fileio import FileIO, BinaryFileIO
 
 
 class Collection:
-    def __init__(self, database, db_name, col_name: str) -> None:
-        self._database = database
-        self._db_name = db_name
+    def __init__(self, col_name: str, binary_file: FileIO) -> None:
         self._col_name = col_name
+        self._binary_file = binary_file
 
-        self._database[self._col_name] = []
-        # self._collection = list(self._database[self._col_name])
+        # Get the data of existing Database or empty database.
+        self._database = self._binary_file.read()
+        # print(self._database)
+        # Initiate a default Collection
 
-    def insert(self, doc: dict) -> None:
-        _doc = doc
-        print(_doc)
-        print(self._database)
-        # data = list(self._collection).append(doc)
-        data = self._database[self._col_name].append(_doc)
-        print(self._database)
-        with open(f"{self._db_name}.json", "w") as f:
-            json.dump(self._database, f, indent=4)
+        # Check the Collection is already exists or no
+        if self._col_name in self._database.keys():
+
+            # Get the existing Collection
+            self._collection: list = self._database[self._col_name]
+
+        else:
+            # Create new Collection
+            self._database[self._col_name] = []
+            self._collection: list = self._database[self._col_name]
+
+    def insert(self, document: dict) -> None:
+        """
+        Inserts a single Document into the Database.
+
+        Document should be JSON Object.
+
+        :param document: Document to insert into database
+        :return: None
+        """
+
+        # Append the document into the Collection
+        self._collection.append(document)
+
+        # Add modified Collection to Database
+        self._database[self._col_name] = self._collection
+        # print(self._database)
+        # Write current state of Database into the Database-file
+        self._binary_file.write(self._database)
+
 
--- filexdb/database.py
+++ filexdb/database.py
@@ -1,11 +1,12 @@
 from typing import Dict, Type, List
 
 from .collection import Collection
+from .fileio import BinaryFileIO
 
 
 class FileXdb:
 
-    def __init__(self, db_name: str, data_dir: str | None):
+    def __init__(self, db_name: str, data_dir: str = None):
         """
         Creates a Databased in ``data_dir`` Directory named ``db_name``.
 
@@ -18,13 +19,23 @@ class FileXdb:
         :param data_dir: Where the Database will be stored.
         """
 
-        self._database = {}
         self._db_name = db_name
         self._data_dir = data_dir
 
+        # Creating an instance of FileIO to Read Write Database-File.
+        self._binary_file = BinaryFileIO(self._db_name, self._data_dir)
+
     def collection(self, col_name: str) -> Collection:
-        collection = Collection(self._database, self._db_name, col_name)
-        # self.database[col_name] = collection
+        """
+        Creates a brand-new Collection if the Collection is not exists.
+
+        If Collection is already exists then it interact with it.
+
+        :param col_name: Collection name to interact with.
+        :return: An instance of Collection Baseclass.
+        """
+        # Initiating collection
+        collection = Collection(col_name, self._binary_file)
 
         return collection
 
