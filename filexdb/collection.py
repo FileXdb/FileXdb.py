d6254275676e8538445cb61280cab258fb05a9a4
Sam <samratbiswas986@gmail.com>
2023-02-23 04:01:50 +0530

    Single & Multi-Insertion Done

--- filexdb/collection.py
+++ filexdb/collection.py
@@ -1,4 +1,5 @@
 import json
+from typing import Mapping, List
 
 from .document import Document
 from .fileio import FileIO, BinaryFileIO
@@ -25,7 +26,7 @@ class Collection:
             self._database[self._col_name] = []
             self._collection: list = self._database[self._col_name]
 
-    def insert(self, document: dict) -> None:
+    def insert(self, document: Mapping) -> int:
         """
         Inserts a single Document into the Database.
 
@@ -34,14 +35,71 @@ class Collection:
         :param document: Document to insert into database
         :return: None
         """
+        # Make sure the document implements the ``Mapping`` interface
+        if not isinstance(document, Mapping):
+            raise ValueError('Document is not a Dictionary')
 
-        # Append the document into the Collection
-        self._collection.append(document)
+        # Create a Document
+        _document = Document(document)
 
-        # Add modified Collection to Database
-        self._database[self._col_name] = self._collection
-        # print(self._database)
-        # Write current state of Database into the Database-file
-        self._binary_file.write(self._database)
+        # Numbers of Document
+        doc_count: int = 0
+
+        # check Document is already exist or not
+        if not self._doc_is_exists(_document.id):
+
+            # Append the document into the Collection
+            self._collection.append(_document)
+
+            # Add modified Collection to Database
+            self._database[self._col_name] = self._collection
+
+            # print(self._database)
+            # Write current state of Database into the Database-file
+            self._binary_file.write(self._database)
+
+            return doc_count + 1
+        else:
+            raise ValueError(f"Document id `{_document.id}` is already exists")
+
+    def insert_all(self, document_list: List[Mapping]) -> int:
+        """
+        Inserts a single ``Document`` into the ``Database``.
+
+        Document should be a ``List`` of ``JSON Object``.
+
+        :param document_list: List of Documents to insert into Database
+        :return: Inserted row(s) count
+        """
+
+        # Make sure the document_list implements the ``List`` interface.
+        if not isinstance(document_list, List):
+            raise ValueError('Document is not a List of Dictionary')
+
+        # Numbers of Document
+        doc_count: int = 0
+
+        # Iterate over all Documents of document_list & Insert one by one.
+        for document in document_list:
+
+            # insert every single document in Database & increment ``doc_count``.
+            doc_count += self.insert(document)
+
+        return doc_count
 
 
+    # ======================== #
+    def _doc_is_exists(self, doc_id: str | int) -> bool:
+        # Iterate over all Documents of Collection
+        for doc in self._collection:
+            if doc["_id_"] == doc_id:
+                return True
+
+        return False
+
+    def _get_document_by_id(self, doc_id) -> Document | str:
+        for doc in self._collection:
+            if doc["_id_"] == doc_id:
+                return doc
+            else:
+                return "none"
--- filexdb/document.py
+++ filexdb/document.py
@@ -1,5 +1,25 @@
+from typing import Mapping
+import uuid
+
+
+def _get_id():
+    _id = uuid.uuid1()
+    return _id.hex
 
 
 class Document(dict):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, value: Mapping) -> None:
+        self.id = None
+
+        _id_obj = {
+            "_id_": _get_id()
+        }
+
+        if "_id_" in value.keys():
+            self._doc = value
+            self.id = value["_id_"]
+        else:
+            self._doc = (_id_obj | value)
+            self.id = self._doc["_id_"]
+
+        super().__init__(self._doc)
